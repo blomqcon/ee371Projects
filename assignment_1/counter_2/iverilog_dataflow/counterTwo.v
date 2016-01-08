b3af1c02b9f688e51869fa2cf53ec3f8410590c6
Connor Blomquist <connor.blomquists@gmail.com>
2016-01-08 11:55:46 -0800

    updated counter 2 to be dataflow model

---
+++ assignment_1/counter_2/iverilog_dataflow/counterTwo.v
@@ -0,0 +1,11 @@
+module CounterTwo(q, clk, rst);
+	input clk, rst;
+	output [3:0] q;
+	
+	wire [3:0] qBar;
+	
+	DFlipFlop c0 (q[0], qBar[0], qBar[0], clk, rst);
+	DFlipFlop c1 (q[1], qBar[1], (qBar[0] & qBar[1]) | (q[1] & q[0]), clk, rst);
+	DFlipFlop c2 (q[2], qBar[2], (qBar[0] & qBar[1] & qBar[2]) | (q[2] & q[0]) | (q[2] & q[1]), clk, rst);
+	DFlipFlop c3 (q[3], qBar[3], (qBar[0] & qBar[1] & qBar[2] & qBar[3]) | (q[3] & q[0]) | (q[3] & q[1]) | (q[3] & q[2]), clk, rst);
+endmodule
