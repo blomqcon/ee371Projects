45f966c0f884da3baad61d90541ab3eaa8cdba74
blomqcon <connor.blomquists@gmail.com>
2016-01-07 21:44:21 -0800

    fixed iverilog counter2

--- assignment_1/counter_2/iverilog/counterTwo.v
+++ assignment_1/counter_2/iverilog/counterTwo.v
@@ -2,15 +2,15 @@ module CounterOne(q, clk, rst);
 	input clk, rst;
 	output [3:0] q;
 	
-	reg ps[0:3]
-	reg ns[0:3]
+	reg ps[3:0]
+	reg ns[3:0]
 
 	always @(*)
 		begin
 			ns[0] <= ~ps[0];
-			ns[1] <= ps[0] xor ps[1];
-			ns[2] <= (ps[0] and ps[1] and ~ps[2]) or (ps[2] and ~ps[0]) or (ps[2] and ~ps[1]);
-			ns[3] <= (ps[3] and ps[2]) or (ps[3] and ps[1]) or (ps[3] and ps[0]) or (ps[0] and ps[1] and ps[2] and ~ps[3]);
+			ns[1] <= ps[0] ^ ps[1];
+			ns[2] <= (ps[0] & ps[1] & ~ps[2]) | (ps[2] & ~ps[0]) | (ps[2] & ~ps[1]);
+			ns[3] <= (ps[3] & ~ps[2]) | (ps[3] & ~ps[1]) | (ps[3] & ~ps[0]) | (ps[0] & ps[1] & ps[2] & ~ps[3]);
 		end
 		
 	assign q = ps;	
@@ -18,7 +18,7 @@ module CounterOne(q, clk, rst);
 		begin
 			if(!rst)
 				begin
-					q <= 0;
+					ps <= 0;
 				end
 			else
 				begin
